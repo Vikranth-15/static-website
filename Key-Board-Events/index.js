27948ca284fdc9efcaffa1aa6871c4aec3e21aef
Vikranth <vikranthyadlapalli123@gmail.com>
2026-02-17 10:42:46 +0530

    Second commit

--- Key-Board-Events/index.js
+++ Key-Board-Events/index.js
@@ -1,7 +1,10 @@
 let inputEl = document.createElement("input");
 
-function printKeyDown() {
+function printKeyDown(event) {
     console.log("Key pressed");
+    console.log(event.type);   //Event Type ---Key Pressed 
+      console.log(event.target);        //Event target --- on which element 
+        console.log(event.key);
 }
 
 inputEl.addEventListener("keydown", printKeyDown);
